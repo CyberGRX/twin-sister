deffca429c9cee91bdb191a2658b6d7b6a6bad73
Charles Burton <charles.burton@cybergrx.com>
2019-03-14 13:40:38 -0600

    feat: enable build pipeline

--- app_build/terraform/main.tf
+++ app_build/terraform/main.tf
@@ -1,5 +1,5 @@
 module "build_pipe" {
-  source          = "git::ssh://git@github.com/CyberGRX/terraform-aws-grx-codebuild.git?ref=v1.3.2"
+  source          = "git::ssh://git@github.com/CyberGRX/terraform-aws-grx-codebuild.git?ref=v1.3.11"
   region          = "${var.region}"
   vpc_id          = "${lookup(data.terraform_remote_state.vpc.vpc_id,var.env)}"
   private_subnets = ["${data.terraform_remote_state.vpc.private_subnets["${var.env}"]}"]
