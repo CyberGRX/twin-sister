2f509e976ac8d5a2fbe04d912ac14666fdcba7ea
Mike Duskis <mike.duskis@cybergrx.com>
2019-05-26 14:41:41 -0600

    Removed cruft

--- upload_package_conditionally.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if test 'master' == "$TARGET_BRANCH"; then
-  twine upload --repository-url $NEXUS_URL -u $NEXUS_USERNAME -p $NEXUS_PASSWORD `ls dist/*.whl | head -n1`
-else
-  echo "Choosing not to upload the package from $TARGET_BRANCH branch to Nexus"
-fi
