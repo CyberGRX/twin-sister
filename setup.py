6338ae50f8fb2c61693593c2b0c900bab9c325d9
Mike Duskis <mike.duskis@cybergrx.com>
2019-06-23 12:28:29 -0600

    Corrected PyPI credential

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from unittest import TestLoader
 
 MAJOR_VERSION = 4
 MINOR_VERSION = 3
-PATCH_VERSION = 4
+PATCH_VERSION = 5
 
 # Environment variable into which CI places the build ID
 # https://docs.gitlab.com/ce/ci/variables/
