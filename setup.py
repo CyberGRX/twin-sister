3e4a7d9040089d39708a7a3b81f309709ff25dc9
Mike Duskis <mike.duskis@cybergrx.com>
2019-06-23 12:37:40 -0600

    More credential escaping games

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from unittest import TestLoader
 
 MAJOR_VERSION = 4
 MINOR_VERSION = 3
-PATCH_VERSION = 6
+PATCH_VERSION = 7
 
 # Environment variable into which CI places the build ID
 # https://docs.gitlab.com/ce/ci/variables/
