59ccbb643ce397ebf401dad419ff2ff9eba6a96a
Mike Duskis <mike.duskis@cybergrx.com>
2019-06-23 12:09:30 -0600

    Version bump for Travis

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from unittest import TestLoader
 
 MAJOR_VERSION = 4
 MINOR_VERSION = 3
-PATCH_VERSION = 1
+PATCH_VERSION = 2
 
 # Environment variable into which CI places the build ID
 # https://docs.gitlab.com/ce/ci/variables/
